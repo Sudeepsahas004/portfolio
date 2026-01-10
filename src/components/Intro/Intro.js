a28b1158843edd2beebef8303a65adf1fd8d3826
Sudeep-Sahas <sudeepsahas004@gmail.com>
2026-01-10 18:56:29 +0530

    Addd cursor

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -7,33 +7,33 @@ import { Link } from "react-scroll";
 const Intro = () => {
   return (
     <section id="intro">
-  
-        <div className="introContent">
-          <span className="hello">Hello, </span>
-          <span className="introText">
-            I'm <span className="introName">Sudeep Sahas</span>
-            <br />
-            Website Designer
-          </span>
-          <span className="introPara">
-            A passionate Web Developer who loves building modern, and
-            <br />
-            responsive websites, creating smooth user experiences.
-          </span>
-          <Link to="contact"
-        smooth={true}
-        offset={-80}
-        duration={100} className="hireBtn">
-            <button className="btn">
-              <img src={hireImg} className="hire" alt="" />
-              <span className="hireMe">Hire Me</span>
-            </button>
-          </Link>
-        </div>
-     
-          <img src={profile} alt="Profile" className="bg" />
-        
-   
+      <div className="introContent">
+        <span className="hello">Hello, </span>
+        <span className="introText">
+          I'm <span className="introName">Sudeep Sahas</span>
+          <br />
+          Website Designer
+        </span>
+        <span className="introPara">
+          A passionate Web Developer who loves building modern, and
+          <br />
+          responsive websites, creating smooth user experiences.
+        </span>
+        <Link
+          to="contact"
+          smooth={true}
+          offset={-80}
+          duration={100}
+          className="hireBtn"
+        >
+          <button className="btn">
+            <img src={hireImg} className="hire" alt="" />
+            <span className="hireMe">Hire Me</span>
+          </button>
+        </Link>
+      </div>
+
+      <img src={profile} alt="Profile" className="bg" />
     </section>
   );
 };
